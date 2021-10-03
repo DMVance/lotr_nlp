5966954ae9cf183b78045b2e93d79c6ca71fd4cf
DMVance <csljrrt@gmail.com>
2021-10-03 07:25:03 -0500

    missing some package components. can't instantiate nlp.

---
+++ nlp_app.py
@@ -0,0 +1,14 @@
+# set up and dependencies
+import pandas as pd
+import numpy as np
+import os, re
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+from flask import Flask, jsonify, render_template
+from datetime import date
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
